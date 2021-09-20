41ca02169733dfa5d4773fcbf49b698793b100fe
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    xaga: sensors: Avoid target name conflicts
    Change-Id: I170c8c7e4edbfcdebf5d6f6df148b72993d805f6
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>

--- device.mk
+++ device.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal
+    android.hardware.sensors@2.1-service.xaga-multihal
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
