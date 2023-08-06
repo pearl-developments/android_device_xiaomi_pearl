b237971b507b4012e7e216c8d656d08a9d7f8f12
ChrisCatto <dontforgetme047@gmail.com>
2023-08-06 17:05:03 +0300

    xaga: configs: Move power configs to device tree
    Signed-off-by: ChrisCatto <dontforgetme047@gmail.com>
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -338,9 +338,12 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-mediatek
 
-    PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
-
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml \
+    $(LOCAL_PATH)/configs/power/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
+    $(LOCAL_PATH)/configs/power/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    
 # Recovery
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6895.rc:recovery/root/init.recovery.mt6895.rc
