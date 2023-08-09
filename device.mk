9d85f45f73530aeb0ff485bca35bff04f5927a76
ChrisCatto <dontforgetme047@gmail.com>
2023-08-09 20:12:56 +0300

    xaga: Fix seperator
    Signed-off-by: ChrisCatto <dontforgetme047@gmail.com>
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -268,7 +268,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     FrameworksResOverlay \
     NotchBarKiller \
-    SettingsOverlay\
+    SettingsOverlay \
     SystemUIOverlay \
     TetheringConfigOverlay \
     WifiOverlay \
