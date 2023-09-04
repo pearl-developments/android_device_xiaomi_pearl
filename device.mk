ea07c8ac46895ae79c11d3275cf162f6cb42e43c
bengris32 <bengris32@protonmail.ch>
2023-09-04 20:04:37 +0100

    xaga: Switch to common USB service
    Change-Id: Ie7bc896c51c435ddd7906f02409c3610386e9ea3
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -471,8 +471,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb-service.mediatek
 
 # Xiaomi Parts
 PRODUCT_PACKAGES += \
